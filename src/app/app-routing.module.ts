c38aeec37a4b45b1586bac0e2b10e910741ab4d1
MiriamS <miriam@salzerdesign.com>
2021-02-11 10:32:01 -0800

    adding listpage component, newcontact component, and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewcontactComponent } from './newcontact/newcontact.component';
+import { ListpageComponent } from './listpage/listpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'newcontact', component: NewcontactComponent },
+    { path: 'list',        component: ListpageComponent },
+    { path: '',   redirectTo: '/list', pathMatch: 'full' },
+    { path: '**', component: ListpageComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/listpage/listpage.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listpage',
+  templateUrl: './listpage.component.html',
+  styleUrls: ['./listpage.component.scss']
+})
+export class ListpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    var contacts = [
+        {
+            "id": 1,
+            "firstname": "Miriam",
+            "lastname": "Salzer",
+            "email": "miriam@msalzer.com",
+            "phone": "650 238 8375"
+        },
+        {
+            "id": 2,
+            "firstname": "Jay",
+            "lastname": "Lo",
+            "email": "jl@example.com",
+            "phone": "650 238 8375"
+        },
+        {
+            "id": 3,
+            "firstname": "Alongerfirstname",
+            "lastname": "Alongerlastname",
+            "email": "aalongerlastname@longerexample.com",
+            "phone": "650 238 8375"
+        },
+        {
+            "id": 4,
+            "firstname": "John",
+            "lastname": "Green",
+            "email": "johngreen@longerexample.com",
+            "phone": "650 238 8375"
+        }
+    ];
+  }
+
+}
